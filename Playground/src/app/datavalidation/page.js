a3eb0d00e0949a9d4b201974d6ac94c535889203
biratdatta <birat.datta26@gmail.com>
2024-06-28 12:55:41 +0530

    feat: add dockerfile and submit issue api

--- Playground/src/app/datavalidation/page.js
+++ Playground/src/app/datavalidation/page.js
@@ -123,10 +123,29 @@ export default function DataValidation() {
     setComment(event.target.value);
   };
 
+
+
   const handleCommentSubmit = () => {
-     
-    console.log('Comment:', comment);
-    handleModalClose();
+    const data = {
+      category: 'validation',
+      text: comment,
+    };
+
+    fetch(`${process.env.NEXT_PUBLIC_REPORT_ISSUE_API}/submit-issue`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      handleModalClose();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
   };
 
   return (
